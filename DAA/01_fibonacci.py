1cb30cf5da906570cbf832f78c9ef83b18c95695
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-11-07 10:37:06 +0000

    new code

--- DAA/01_fibonacci.py
+++ DAA/01_fibonacci.py
@@ -32,3 +32,45 @@ if __name__ == '__main__':
     print("Steps:", fibonacci_iter(n)[1])
     print("Recursive:", fibonacci_recur(n)[0])
     print("Steps:", fibonacci_recur(n)[1])
+
+# way 2nd
+
+#Non-recursion
+nterms = int(input("How many terms? "))
+# first two terms 
+n1, n2 = 0, 1
+count = 0
+# check if the number of terms is valid 
+if nterms <= 0:
+  print("Please enter a positive integer") 
+# if there is only one term, return n1 
+elif nterms == 1:
+  print("Fibonacci sequence upto",nterms,":") 
+  print(n1)
+# generate fibonacci sequence 
+else:
+  print("Fibonacci sequence:") 
+while count < nterms:
+    print(n1)
+    nth = n1 + n2
+    # update values 
+    n1 = n2
+    n2 = nth 
+    count += 1
+
+#recursion
+
+def recur_fibo(n):
+    if n <= 1: 
+       return n
+    else:
+       return(recur_fibo(n-1) + recur_fibo(n-2)) 
+
+nterms = 7
+
+if nterms <= 0:
+  print("Plese enter a positive integer") 
+else:
+    print("Fibonacci sequence:") 
+    for i in range(nterms):
+      print(recur_fibo(i))
